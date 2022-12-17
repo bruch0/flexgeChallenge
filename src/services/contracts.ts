f99e9aaae7dbdf5348eb848d64c8659a7a55c711
bruch0 <lucas.bruch0@gmail.com>
2022-12-17 00:48:39 -0300

    fix: remove console.log

--- src/services/contracts.ts
+++ src/services/contracts.ts
@@ -2,7 +2,6 @@ import * as contractRepository from '@repositories/contracts';
 
 const getAllContracts = async (): Promise<any> => {
   const contracts = await contractRepository.getAllContracts();
-  console.log('oiw!');
 
   return contracts;
 };
