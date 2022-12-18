94d5b820e98dff5d550f01952f7a8cb21d177a29
bruch0 <lucas.bruch0@gmail.com>
2022-12-17 23:31:49 -0300

    chore: add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,27 @@
+export interface ProductInterface {
+  name: string;
+  amount: Number;
+  unitPrice: Number;
+  installments: Number;
+  paidInstallments: Number;
+  termBegin: Date;
+}
+export interface Contracts {
+  country: String;
+  state: string;
+  city: string;
+  documentId: string;
+  socialReason: string;
+  address: string;
+  district: string;
+  number: Number;
+  zipcode: string;
+  email: string;
+  phone: string;
+  startDate: Date;
+  endDate: Date;
+  dueDay: Date;
+  contractUrl: string;
+  companyName: string;
+  products: [ProductInterface];
+}
